6bcebbbc865b8ade19b7484e7e6cc44db2dc0163
saranyasaju-ui <saranyasaju.kathirvel@gmail.com>
2025-04-03 14:58:13 +0530

    updated Home file

---
+++ Views/Home/create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExampleRepo.Views.Home
+{
+    public class createModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
